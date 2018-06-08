94f30a4917945038067f4215a4a1cc51d96b15d7
kersha38 <carlos.gutierrez@epn.edu.ec>
2018-06-07 21:16:12 -0500

    Pao tenía razón

--- app/src/main/java/com/example/carlos/proyectomascotas/MenuActivity.java
+++ app/src/main/java/com/example/carlos/proyectomascotas/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.carlos.proyectomascotas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void irAComida(View view){
+        Intent intent = new Intent(getApplicationContext(), ComidaActivity.class);
+        startActivity(intent);
+    }
+
+    public void irAAgua(View view){
+        Intent intent = new Intent(getApplicationContext(), AguaActivity.class);
+        startActivity(intent);
     }
 }
