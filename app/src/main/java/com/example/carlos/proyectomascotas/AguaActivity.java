5891153c992e3fad8bea4140b383111ed9df3e53
paosteph <pao_st55@hotmail.com>
2018-06-08 10:56:12 -0500

    bontones imagenes

--- app/src/main/java/com/example/carlos/proyectomascotas/AguaActivity.java
+++ app/src/main/java/com/example/carlos/proyectomascotas/AguaActivity.java
@@ -1,5 +1,7 @@
 package com.example.carlos.proyectomascotas;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -46,6 +48,48 @@ public class AguaActivity extends AppCompatActivity {
 
     public void llenarAgua(View view){
         //configuracion raspberry liberar agua ml
-        Toast.makeText(getApplicationContext(),"Recipiente con agua", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(),"Recipiente con agua", Toast.LENGTH_SHORT).show();
+        crearDialogoAlert();
+    }
+
+    public void crearDialogoAlert(){
+        // Builder para crear la alerta
+        // this = getaplicationcontext
+        AlertDialog.Builder dialogoAlerta= new AlertDialog.Builder(getApplicationContext());
+        dialogoAlerta.setTitle("ALERTA PARA LLENAR AGUA");
+        dialogoAlerta.setMessage("¿Esta seguro de llenar recipiente con agua?");
+
+        // interface para el boton Positive
+        dialogoAlerta.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //lo q sucede al dar clic
+                Toast.makeText(getApplicationContext(),"Recipiente con agua",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // interface para el boton Negative
+        dialogoAlerta.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //lo q sucede al dar clic
+                //Toast.makeText(getApplicationContext(),"N",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // interface para el boton Neutral
+        dialogoAlerta.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //lo q sucede al dar clic
+                //Toast.makeText(getApplicationContext(),"Selecciono Neutral",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // cancelar sin aplastar un boton
+        dialogoAlerta.setCancelable(true);
+
+        dialogoAlerta.create();
+        dialogoAlerta.show();
     }
 }
