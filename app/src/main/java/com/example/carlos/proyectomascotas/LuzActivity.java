63ff631d4b2f5a50e69e0649c4abb89e11ff8414
Paolita <pao_st55@hotmail.com>
2018-06-13 01:26:30 -0500

    layout parte 3

--- app/src/main/java/com/example/carlos/proyectomascotas/LuzActivity.java
+++ app/src/main/java/com/example/carlos/proyectomascotas/LuzActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,7 +13,8 @@ import java.util.Locale;
 
 public class LuzActivity extends AppCompatActivity {
     TextView txtEstadoIluminacion;
-    Button btnEncenderApagar;
+    ImageView imgEncenderApagar;
+    Switch switchLuz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +22,24 @@ public class LuzActivity extends AppCompatActivity {
         setContentView(R.layout.activity_luz);
 
         txtEstadoIluminacion=(TextView)findViewById(R.id.textEstadoIluminacion);
-        btnEncenderApagar=(Button)findViewById(R.id.buttonEncenderApagar);
+        imgEncenderApagar=(ImageView)findViewById(R.id.imageEncenderApagar);
+        switchLuz = (Switch)findViewById(R.id.switchLuz);
 
-        btnEncenderApagar.setOnClickListener(new View.OnClickListener() {
+        switchLuz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // envio peticion al servior
                 //Toast.makeText(getApplicationContext(),"Orden Enviada",Toast.LENGTH_SHORT).show();
 
                 //datos quemados cambiar
-                if(txtEstadoIluminacion.getText()=="apagado"){
+                if(switchLuz.isChecked()){
                     txtEstadoIluminacion.setText("encendido");
-                    btnEncenderApagar.setBackgroundResource(R.drawable.focoencendido);
+                    imgEncenderApagar.setBackgroundResource(R.drawable.focoencendido);
                 }else{
                     txtEstadoIluminacion.setText("apagado");
-                    btnEncenderApagar.setBackgroundResource(R.drawable.focoapagado);
+                    imgEncenderApagar.setBackgroundResource(R.drawable.focoapagado);
                 }
+
             }
         });
     }
